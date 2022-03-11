9d54696d10bd8584e16a6f8f9aeb7e2b78bd25a9
Fernando Mumbach <fermuch@ayvu.net>
2022-03-11 16:53:51 -0300

    adding configuration to select which outputs to enable/disable when locking/unlocking

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 import * as MonoUtils from "@fermuch/monoutils";
+import { getBoolean } from "@fermuch/monoutils/build/types/tools/storage";
 
 // based on settingsSchema @ package.json
 type Config = Record<string, unknown> & {
-  nome: string;
+  target: ('MONOFLOW_RELAY_1' | 'MONOFLOW_RELAY_2' | 'MONOFLOW_BUZ_1')[];
 }
-
 const conf = new MonoUtils.config.Config<Config>();
 
-messages.on('onInit', function() {
-  platform.log('Lock/unlock script installed.');
-});
-
-messages.on('onEvent', (event: any) => {
-  if (event?.kind === 'lock-request') {
-    const ev = event as MonoUtils.wk.lock.LockEvent;
-    const locked = ev.getData()?.lock || true;
-    platform.log('Lock/unlock (messages) script: ' + (locked ? 'Locked' : 'Unlocked'));
-  } else {
-    platform.log('received message onEvent: ' + JSON.stringify(event));
+function setLock(state: boolean) {
+  let targets = conf.get("target", ['MONOFLOW_RELAY_1']);
+  if (!Array.isArray(targets)) {
+    targets = [targets];
   }
-})
+
+  targets.forEach(target => {
+    env.setData(target, Boolean(state));
+  });
+}
+
+messages.on('onInit', function() {
+  platform.log('Lock/unlock script installed. Applying current lock state...');
+
+  const lastState = MonoUtils.wk.lock.getLockState();
+  setLock(lastState);
+});
 
 MonoUtils.wk.event.subscribe<MonoUtils.wk.lock.LockEvent>('lock-request', (ev) => {
   const locked = ev.getData()?.lock || true;
   platform.log('Lock/unlock script: ' + (locked ? 'Locked' : 'Unlocked'));
+  setLock(locked);
 })
